292a83239fbe6beec2b0d7e6666737b2c0df5e2a
Vinícius Figueiroa <vinissfigueiroa@gmail.com>
2021-07-19 03:01:29 -0300

    Fix

--- pages/api/scraps.js
+++ pages/api/scraps.js
@@ -2,8 +2,7 @@ import { SiteClient } from 'datocms-client';
 
 export default async function getRequests(req, res) {
   if (req.method === 'POST') {
-    const TOKEN = process.env.API_KEY;
-    const client = new SiteClient(TOKEN);
+    const client = new SiteClient(process.env.API_KEY);
 
     const record = await client.items.create({
       itemType: '972394', //Id do Model 'Scrap' criado lá no DatoCMS
